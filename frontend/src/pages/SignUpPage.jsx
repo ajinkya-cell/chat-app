50cc595e0359912e37f55fc2ae69defc36a7423a
ajinkya-cell <ajinkyaadharkar@gmail.com>
2026-01-05 10:54:34 +0530

    corrected the code

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import { useState } from 'react';
-import { useAuthStore } from '../store/useAuthStore';
-import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User} from "lucide-react"
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
-import { AuthImagePattern } from '../components/AuthImagePattern';
+import {AuthImagePattern} from "../components/AuthImagePattern";
+import toast from "react-hot-toast";
+
 const SignUpPage = () => {
-  const [showPassword , setShowPassword] = useState(false);
-  const [formData , setFormData] = useState({
-    fullName : "",
-    email : "",
-    password : "",
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
   });
 
-  const {signup , isSigningUp} = useAuthStore();
-  const validateForm = () =>{
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
     if (!formData.fullName.trim()) return toast.error("Full name is required");
     if (!formData.email.trim()) return toast.error("Email is required");
     if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
     if (!formData.password) return toast.error("Password is required");
     if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
 
-
     return true;
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  };
 
-    const success =validateForm()
-    if(success ===true) signup(formData)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const success = validateForm();
+
+    if (success === true) signup(formData);
+  };
+
   return (
-    <div className=' min-h-screen grid lg:grid-cols-2'>
-      <div className='flex flex-col items-center p-6 sm:p-12'>
-        <div className='w-full max-w-md space-y-8'>
-        <div className="text-center mb-8">
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* LOGO */}
+          <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
@@ -47,8 +53,8 @@ const SignUpPage = () => {
             </div>
           </div>
 
-          <form onSubmit={handleSubmit} className='space-y-6'>
-          <div className="form-control">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Full Name</span>
               </label>
@@ -65,6 +71,7 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Email</span>
@@ -82,6 +89,7 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-medium">Password</span>
@@ -122,6 +130,7 @@ const SignUpPage = () => {
               )}
             </button>
           </form>
+
           <div className="text-center">
             <p className="text-base-content/60">
               Already have an account?{" "}
@@ -132,12 +141,14 @@ const SignUpPage = () => {
           </div>
         </div>
       </div>
+
+      {/* right side */}
+
       <AuthImagePattern
         title="Join our community"
         subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
       />
     </div>
-  )
-}
-
-export default SignUpPage
+  );
+};
+export default SignUpPage;
